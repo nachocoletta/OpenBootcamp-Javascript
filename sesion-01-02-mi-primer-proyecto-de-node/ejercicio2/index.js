0fe6cdda3b58219e596d8def23621bf8f57ac9af
Nacho <ignacio_coletta@hotmail.com>
2022-09-18 15:14:23 -0300

    unidad 2

---
+++ sesion-01-02-mi-primer-proyecto-de-node/ejercicio2/index.js
@@ -0,0 +1,6 @@
+var misDatos = ["Nacho", 
+                44, 
+                false, 
+                {titulo: "Cementario de Animales", autor: "Stephen King", fecha: new Date('1984/11/14'), url: "https://es.wikipedia.org/wiki/Cementerio_de_animales"}]
+
+console.log(misDatos);

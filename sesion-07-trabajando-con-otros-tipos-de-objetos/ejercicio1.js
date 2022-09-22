4c32ad25532dc84b3c25a36869beb34bb991e24d
Nacho <ignacio_coletta@hotmail.com>
2022-09-22 10:54:03 -0300

    sesion 07 ejercicio 1

---
+++ sesion-07-trabajando-con-otros-tipos-de-objetos/ejercicio1.js
@@ -0,0 +1,13 @@
+const familiares = ['Osvaldo', 'Noemi', 'Vanina', 'Ileana', 'Paula', 'Joaquin', 'Nacho']
+
+const setFamiliares = new Set(familiares);
+
+console.log(setFamiliares)
+
+setFamiliares.add('Nacho');
+
+console.log(setFamiliares);
+
+setFamiliares.add('Javascript');
+
+console.log(setFamiliares);

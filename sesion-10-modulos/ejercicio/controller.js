7b0d974955bfc817e67e2a80145dc156181a5c50
Nacho <ignacio_coletta@hotmail.com>
2022-09-26 16:00:48 -0300

    sesion-10

---
+++ sesion-10-modulos/ejercicio/controller.js
@@ -0,0 +1,8 @@
+// - Crea un archivo controller.js que exporte 2 funciones: suma(a, b) y multiplica(a, b)
+export const suma = (a, b) => {
+    return a + b;
+}
+
+export const multiplica = (a, b) => {
+    return a * b;
+}
